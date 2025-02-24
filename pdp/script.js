c6e5f1b8c3856a0a85b7b3c318e81fe89981cf9b
ram <ram.landge009@gmail.com>
2025-02-24 20:16:28 +0530

    new commit-till accordian

--- pdp/script.js
+++ pdp/script.js
@@ -1,44 +1,11 @@
-// function changeMainImage(src) {
-//   document.getElementById("mainImage").src = src;
-// }
-// const scrollUpButton = document.getElementById("scrollUp");
-// const scrollDownButton = document.getElementById("scrollDown");
-// const sliderNav = document.getElementById("sliderNav");
-
-// scrollUpButton.addEventListener("click", () => {
-//   sliderNav.scrollBy(0, -100);
-// });
-
-// scrollDownButton.addEventListener("click", () => {
-//   sliderNav.scrollBy(0, 100);
-// });
-
-const scrollUpButton = document.getElementById("scrollUp");
-const scrollDownButton = document.getElementById("scrollDown");
-const thumbnails = document.querySelectorAll(".thumbnail");
-let startIndex = 0;
-const thumbnailsPerPage = 6;
-
-function updateThumbnailVisibility() {
-  thumbnails.forEach((thumbnail, index) => {
-    thumbnail.parentElement.classList.toggle(
-      "hidden",
-      index < startIndex || index >= startIndex + thumbnailsPerPage
-    );
-  });
+function increaseQuantity(button) {
+  const input = button.previousElementSibling;
+  input.value = parseInt(input.value) + 1;
 }
 
-scrollUpButton.addEventListener("click", () => {
-  startIndex = Math.max(startIndex - thumbnailsPerPage, 0);
-  updateThumbnailVisibility();
-});
-
-scrollDownButton.addEventListener("click", () => {
-  startIndex = Math.min(
-    startIndex + thumbnailsPerPage,
-    thumbnails.length - thumbnailsPerPage
-  );
-  updateThumbnailVisibility();
-});
-
-updateThumbnailVisibility();
+function decreaseQuantity(button) {
+  const input = button.nextElementSibling;
+  if (parseInt(input.value) > 1) {
+    input.value = parseInt(input.value) - 1;
+  }
+}
